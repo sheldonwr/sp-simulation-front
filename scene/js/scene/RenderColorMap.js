1bedca5493d58d77ab99fc78b9b1926e5bf93779
zongjie <zongjie.wxt@xuelangyun.com>
2021-11-29 22:25:01 +0800

    recompute color map width

--- scene/js/scene/RenderColorMap.js
+++ scene/js/scene/RenderColorMap.js
@@ -5,13 +5,15 @@ export default function ({ lut, controls }) {
   const colorWidth = document.getElementById('visual-map').clientWidth * 0.5;
   const colorTextHeight = document.getElementById('color-text').clientHeight * 0.5;
   const colorTextWidth = document.getElementById('color-text').clientWidth * 0.5;
-  var visualMap = d3.select("#visual-map").append("svg").attr('height', colorHeight).attr('width', colorWidth);
-  var colorText = d3.select("#color-text").append("svg").attr('height', colorTextHeight).attr('width', colorTextWidth);
+  
   var step = 0.05;
   var count = controls.BendMax / step;
-  const textCountBreak = Math.floor(count * 3);
+  const textCountBreak = Math.floor(count * 5);
   const barWidth = Math.floor(colorWidth / count);
   const fontSize = 16;
+
+  var visualMap = d3.select("#visual-map").append("svg").attr('height', colorHeight).attr('width', colorWidth);
+  var colorText = d3.select("#color-text").append("svg").attr('height', colorTextHeight).attr('width', colorTextWidth + 100);
 
   for (let index = 0, colorTextCounter = 0; index < count; index += step, colorTextCounter++) {
     const colorValue = (index * controls.BendMax / count).toFixed(4);
@@ -20,7 +22,7 @@ export default function ({ lut, controls }) {
 
     if (colorTextCounter === textCountBreak || colorTextCounter === 0) {
       colorTextCounter = 0;
-      colorText.append('text').attr('x', index * barWidth).attr('y', fontSize).text(colorValue).style('font-size', fontSize).style('fill', color);
+      colorText.append('text').attr('x', (index + 1) * barWidth).attr('y', fontSize).text(colorValue).style('font-size', fontSize).style('fill', color);
     }
   }
 }
