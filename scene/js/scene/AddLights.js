1c38d7667eded5edd8e389bcb0b588a32d5cc9d0
zongjie <zongjie.wxt@xuelangyun.com>
2021-11-29 22:24:52 +0800

    reduce shadow map size

--- scene/js/scene/AddLights.js
+++ scene/js/scene/AddLights.js
@@ -1,10 +1,10 @@
 import * as THREE from '../../libs/threejs/three.module.js';
 
 export default function (scene) {
-  scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+  scene.add(new THREE.AmbientLight(0xffffff, 0.8));
 
   var directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(-20, 30, -20);
+  directionalLight.position.set(-10, 10, -10);
   directionalLight.castShadow = true;
   directionalLight.shadow.camera.near = 2;
   directionalLight.shadow.camera.far = 80;
@@ -14,8 +14,8 @@ export default function (scene) {
   directionalLight.shadow.camera.bottom = -50;
 
   directionalLight.intensity = 1;
-  directionalLight.shadow.mapSize.width = 2048;
-  directionalLight.shadow.mapSize.height = 2048;
+  directionalLight.shadow.mapSize.width = 512;
+  directionalLight.shadow.mapSize.height = 512;
 
   scene.add(directionalLight);
 }
